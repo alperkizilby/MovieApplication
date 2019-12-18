a44740b9713f42fd9310691d6baa61bb8c9fa540
kayakadir <kayakadir@users.noreply.github.com>
2019-12-18 11:13:23 +0300

    Update index.js
    yorum satırı eklendi.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry} from 'react-native';//react native app
 import App from './App';// aktarım yapılır
 import {name as appName} from './app.json';
 
